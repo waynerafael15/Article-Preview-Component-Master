a2c5b0854c21c2dd67203abc46066d8c7cfe8404
Wayne Rodriguez <wayenerafael15@gmail.com>
2021-11-03 10:06:43 -0400

    My Card V1

---
+++ app.js
@@ -0,0 +1,10 @@
+let shareBtn = document.querySelector(".article__icon");
+let Share = document.querySelector('.share');
+
+// console.log(shareBtn);
+// console.log(Share)
+
+shareBtn.addEventListener('click', function(){
+    shareBtn.classList.toggle("change");
+    Share.classList.toggle("change");
+})
